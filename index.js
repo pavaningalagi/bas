604e3cac746a2b06e708db45f5deb171588db583
Pavan Ingalagi <ingalagipavan@gmail.com>
2023-03-20 01:40:03 +0530

    node

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const port = 8081;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hello World! 2"));
 app.get("/about", (req, res) => res.send("your in server one"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
